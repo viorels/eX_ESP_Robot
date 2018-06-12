6ea2ec3d096f7ff4837ef998f2c9792b846e1d3b
Viorel Stirbu <viorels@gmail.com>
2018-06-12 17:48:53 +0300

    custom pins

--- eX_ESP_Pins.h
+++ eX_ESP_Pins.h
@@ -16,41 +16,24 @@
 // GPIO12   : v
 // GPIO13   : v
 // GPIO14   : v
-// GPIO15   : v стартовые условия - 10К на землю
+// GPIO15   : v starting conditions - 10K on the ground
 // GPIO16   : v
 // T_OUT    : v Analog pin (17)
 
-#ifdef ORIGINAL_PINS
-#define MOTORS_ENABLE_PIN    15 // резистор 10К на землю может не устанавливаться, так как резисторы интегрированы в драйвера ШД
-#define MOTORS_DIR_PIN       14
-#define MOTOR1_STEP_PIN      12
-#define MOTOR2_STEP_PIN      13
+#define MOTORS_ENABLE_PIN    D8 // A 10k resistor is not nécessary as integrated inside the motor driver
+#define MOTORS_DIR_PIN       D7
+#define MOTOR1_STEP_PIN      D5
+#define MOTOR2_STEP_PIN      D6
 
-#define SONAR_TRIG_PIN       16 // !!! этот и никакой другой !!!
-#define SONAR_ECHO_PIN       0
-#define SDA_PIN              4 // или 5, для ESP-07 (плата разведена не правильно)
-#define SCL_PIN              5 // или 4, для ESP-07 (плата разведена не правильно)
+#define SONAR_TRIG_PIN       16 // D0 !!! this and no other !!!
+#define SONAR_ECHO_PIN       0  // D3
+#define SDA_PIN              4  // D2
+#define SCL_PIN              5  // D1
 
-#define SERVO1_PIN           2
-#define SERVO2_PIN           3
-#define BATTERY_PIN          17
+#define SERVO1_PIN           2  // D4
+#define SERVO2_PIN           3  // RX
+#define BATTERY_PIN          17 // A0?
 
-#else // rolfz pinout
-#define MOTORS_ENABLE_PIN    12 //D6 //rz: A 10k resistor is not nécessary as integrated inside the motor driver
-#define MOTORS_DIR_PIN       15 //D8  //14
-#define MOTOR1_STEP_PIN      13 //D7
-#define MOTOR2_STEP_PIN      14 //D5
-
-#define SONAR_TRIG_PIN       -1 //16 // NO sonar in my first check
-#define SONAR_ECHO_PIN       -1 //0
-#define SDA_PIN              4
-#define SCL_PIN              5
-
-#define SERVO1_PIN           2
-#define SERVO2_PIN           3
-#define BATTERY_PIN          17
- 
 #define LED_RED              16 //rz: This to check gyro trouble as gyro gets in trouble during code upload
-#endif
 
 #endif //EX_ESP_PINS_H
